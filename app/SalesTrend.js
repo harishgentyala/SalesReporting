a9aea44ea564aaaa64e10379b160e8c77efefcaa
Gentyala <hg185040@ncr.com>
2018-09-26 19:01:15 +0530

    added react native chart kit library and integrated in the salesTrend

--- app/SalesTrend.js
+++ app/SalesTrend.js
@@ -1,5 +1,13 @@
 import React from 'react'
-import { LineChart, Grid } from 'react-native-svg-charts'
+import {
+    LineChart,
+    BarChart,
+    PieChart,
+    ProgressChart,
+    ContributionGraph
+} from 'react-native-chart-kit';
+import { Dimensions, Text, View } from 'react-native';
+const screenWidth = Dimensions.get('window').width
 
 class SalesTrend extends React.PureComponent {
 
@@ -28,11 +36,13 @@ class SalesTrend extends React.PureComponent {
 .then((response) => response.json())
 .then((responseJson) => {
 
-    this.setState({
-    data: responseJson
-    }, function(){
+    if(!responseJson.exception) {
+        this.setState({
+            data: responseJson
+        }, function () {
 
-});
+        });
+    }
 
 })
 .catch((error) =>{
@@ -41,19 +51,39 @@ class SalesTrend extends React.PureComponent {
 }
 
     render() {
-
-        const resultantData = this.state.data.results.map((element,index) => parseFloat(element[3]));
+        console.log("resultant data",this.state.data.results);
+        const resultantData = this.state.data.results.map((element,index) => parseInt(element[3]));
+        const data = {
+            labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+            datasets: [{
+                data: resultantData//[ 20, 45, 28, 80, 99, 43 ]
+            }]
+        };
+        console.log("final data",data.datasets[0].data);
 
         return (
+            <View>
+        {data.datasets[0].data.length > 0 ? (
             <LineChart
-                style={{ height: 200 }}
-                data={ resultantData }
-                svg={{ stroke: 'rgb(134, 65, 244)' }}
-                contentInset={{ top: 20, bottom: 20 }}
-            >
-                <Grid/>
-            </LineChart>
-        )
+                data={data}
+                bezier = {true}
+                width={screenWidth}
+                height={220}
+                chartConfig={{
+                  backgroundColor: '#e26a00',
+                  backgroundGradientFrom: '#fb8c00',
+                  backgroundGradientTo: '#ffa726',
+                  color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
+                  style: {
+                    borderRadius: 16
+                  }
+                }}
+            />
+        ) : (
+            <Text>No data to display</Text>
+        )}
+                </View>
+        );
     }
 }
 
