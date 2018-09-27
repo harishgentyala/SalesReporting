a2469b569dee8dcc503616f6278ae9ee0fb1d106
Gentyala <hg185040@ncr.com>
2018-09-27 16:36:35 +0530

    Merge branch 'rajesh_work' of https://github.com/harishgentyala/SalesReporting into rajesh_work
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- app/BusyHours.js
+++ app/BusyHours.js
@@ -1,42 +1,57 @@
 import React, { Component } from 'react';
-import {View, Dimensions} from 'react-native';
+import {ActivityIndicator, View, Dimensions, Text, TouchableOpacity} from 'react-native';
+import IOSIcon from "react-native-vector-icons/Ionicons";
 import {BarChart} from 'react-native-chart-kit'
 
 const window = Dimensions.get('window');
 const screenWidth = window.width
-const data = {
-  labels: ['January', 'February', 'March', 'April', 'May', 'June'],
-  datasets: [{
-    data: [ 20, 45, 28, 80, 99, 43 ]
-  }]
-}
 
 class BusyHours extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      data: [],
+      isLoading: true,
+      data: {results:[]},
     };
   }
-
+ 
   componentDidMount() {
-    this.generateData();
+    this.getData();
   }
 
-  componentWillUnmount() {
-    clearInterval(this.interval);
-  }
-
-  generateData = () => {
-    const data = [];
-    for (let i = 0; i < 10; i++) {
-      data.push(Math.floor(Math.random()*500));
+  getData = () => {
+    options = {
+        headers:{
+            "Authorization":"Basic YWRtaW46S1lMSU4=",
+            "Content-Type":"application/json"
+        },
+        method: "POST",
+        body: JSON.stringify({
+            "sql":"SELECT \"HOUR\", COUNT(SALESREPORT_INFO.CUSTOMERID) FROM  SALESREPORT_INFO WHERE SALESREPORT_INFO.BUSINESSDATE IN('2018-09-26') GROUP BY \"HOUR\" ORDER BY \"HOUR\"",
+            "offset":0,
+            "limit":9,
+            "acceptPartial":false,
+            "project":"SALESREPORTINGAPP"
+        })
     }
 
-    this.setState({
-      data,
+    return fetch('http://153.71.16.34:7070/kylin/api/query',options)
+    .then((response) => response.json())
+    .then((responseJson) => {
+        if(!responseJson.exception) {
+            this.setState({
+                isLoading: false,
+                data: responseJson
+            }, function () {
+
+            });
+        }
+    })
+    .catch((error) =>{
+        console.error(error);
     });
+
   }
 
   render() {
@@ -45,35 +60,44 @@ class BusyHours extends Component {
       borderBottomRightRadius: 5
     };
 
+    const resultCustomers = this.state.data.results.map((element,index) => parseInt(element[1]));
+    const resultantHours = this.state.data.results.map((element,index) => element[0]);
     const data = {
-        labels: ['January', 'February', 'March', 'April', 'May', 'June'],
+        labels: resultantHours,
         datasets: [{
-            data: [ 20, 45, 28, 80, 99, 43 ]
+            data: resultCustomers
         }]
     };
 
     return (
-      <View style={{flexDirection: 'row', backgroundColor: '#F5FCFF'}}>
-          <BarChart
-            style={{
-      marginVertical: 8,
-      borderRadius: 16
-    }}
-            data={data}
-            width={screenWidth}
-            height={220}
-            chartConfig={{
-                backgroundColor: '#fdfdfd',
-                backgroundGradientFrom: '#adadad',
-                backgroundGradientTo: '#efefef',
-                color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-                style: {
-                    borderRadius: 5
-                }
-            }}
-            />
-      </View>
-    );
+        <View>
+            <TouchableOpacity onPress= {() => this.props.navigation.navigate("SalesTrendFilters",{
+                onNavigateBack: this.handleOnNavigateBack.bind(this)
+            })}>
+                <IOSIcon name="ios-menu" size={30} />
+            </TouchableOpacity>
+        {this.state.isLoading ? <View style={{marginTop: 100}}><ActivityIndicator size="large" color="#0000ff" /></View> : (data.datasets[0].data.length > 0 ? 
+            <View style={{flexDirection: 'row'}}>
+                <BarChart
+                    style={{
+                        marginVertical: 1,
+                        borderRadius: 2
+                    }}
+                    data={data}
+                    width={screenWidth}
+                    height={300}
+                    chartConfig={{
+                        backgroundColor: '#ffffff',
+                        backgroundGradientFrom: '#ffffff',
+                        backgroundGradientTo: '#ffff',
+                        color: (opacity = 1) => `rgba(20, 99, 9, 1)`,
+                        style: {
+                            borderRadius: 1
+                        }
+                    }}/>
+            </View> :
+            <Text>No data to display</Text>)}
+        </View>);
   }
 }
 
