b55d76e2e390ee41197ed70f57080df2e3059821
Adapa <ra185170@ncr.com>
2018-09-27 14:22:09 +0530

    Busy Hours Screen changes

--- app/BusyHours.js
+++ app/BusyHours.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {View, Dimensions} from 'react-native';
+import {ActivityIndicator, View, Dimensions} from 'react-native';
 import {BarChart} from 'react-native-chart-kit'
 
 const window = Dimensions.get('window');
@@ -16,27 +16,50 @@ class BusyHours extends Component {
     super(props);
 
     this.state = {
+      isLoading: true,
       data: [],
     };
   }
 
   componentDidMount() {
-    this.generateData();
+    this.getData();
   }
 
   componentWillUnmount() {
-    clearInterval(this.interval);
+
   }
 
-  generateData = () => {
-    const data = [];
-    for (let i = 0; i < 10; i++) {
-      data.push(Math.floor(Math.random()*500));
+  getData = () => {
+    options = {
+        headers:{
+            "Authorization":"Basic YWRtaW46S1lMSU4=",
+            "Content-Type":"application/json"
+        },
+        method: "POST",
+        body: JSON.stringify({
+            "sql":"SELECT \"HOUR\", COUNT(DISTINCT(SALESREPORT_INFO.CUSTOMERID)) FROM  SALESREPORT_INFO WHERE SALESREPORT_INFO.BUSINESSDATE IN('2018-09-26') GROUP BY \"HOUR\" ORDER BY \"HOUR\"",
+            "offset":0,
+            "limit":50000,
+            "acceptPartial":false,
+            "project":"SALESREPORTINGAPP"
+        })
     }
 
-    this.setState({
-      data,
+    return fetch('http://153.71.16.34:7070/kylin/api/query',options)
+    .then((response) => response.json())
+    .then((responseJson) => {
+        if(!responseJson.exception) {
+            this.setState({
+                data: responseJson
+            }, function () {
+
+            });
+        }
+    })
+    .catch((error) =>{
+        console.error(error);
     });
+
   }
 
   render() {
@@ -45,35 +68,39 @@ class BusyHours extends Component {
       borderBottomRightRadius: 5
     };
 
+    //const resultCustomers = this.state.data.results.map((element,index) => parseInt(element[1]));
+     //   const resultantHours = this.state.data.results.map((element,index) => element[0]);
     const data = {
         labels: ['January', 'February', 'March', 'April', 'May', 'June'],
         datasets: [{
             data: [ 20, 45, 28, 80, 99, 43 ]
         }]
-    };
+    }
 
     return (
-      <View style={{flexDirection: 'row', backgroundColor: '#F5FCFF'}}>
-          <BarChart
-            style={{
-      marginVertical: 8,
-      borderRadius: 16
-    }}
-            data={data}
-            width={screenWidth}
-            height={220}
-            chartConfig={{
-                backgroundColor: '#fdfdfd',
-                backgroundGradientFrom: '#adadad',
-                backgroundGradientTo: '#efefef',
-                color: (opacity = 1) => `rgba(255, 255, 255, ${opacity})`,
-                style: {
-                    borderRadius: 5
-                }
-            }}
-            />
-      </View>
-    );
+        <View>
+        {data.datasets[0].data.length > 0 ? 
+            <View style={{flexDirection: 'row', backgroundColor: '#F5FCFF'}}>
+                <BarChart
+                    style={{
+                        marginVertical: 8,
+                        borderRadius: 16
+                    }}
+                    data={data}
+                    width={screenWidth}
+                    height={220}
+                    chartConfig={{
+                        backgroundColor: '#fdfdfd',
+                        backgroundGradientFrom: '#adadad',
+                        backgroundGradientTo: '#efefef',
+                        color: (opacity = 1) => `rgba(0, 0, 0, 1)`,
+                        style: {
+                            borderRadius: 5
+                        }
+                    }}/>
+            </View> :
+            <Text>No data to display</Text>}
+        </View>);
   }
 }
 
